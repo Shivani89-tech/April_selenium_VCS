67fff2da65227903c9b021add668fa3347b75620
hp <hp@LAPTOP-B4GPSVUL>
2021-08-02 23:29:51 +0530

    [shivani]:task 2 is completed..

--- src/sample1/demo1.java
+++ src/sample1/demo1.java
@@ -2,8 +2,9 @@ package sample1;
 
 public class demo1 {
 	public static void main(String[] args) {
-		System.out.println("hi..");
+		System.out.println("hey..");
 		System.out.println("shivani");
+		System.out.println("you are gorgious girl");
 	}
 
 }
